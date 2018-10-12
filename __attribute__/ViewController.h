4aacda6a9579da4162f2ccfaff5a313f6673d69b
Brian.Chu <chuchengpeng0119@163.com>
2018-10-12 17:06:32 +0800

    Initial Commit

---
+++ __attribute__/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  __attribute__
+//
+//  Created by 储诚鹏 on 2018/10/12.
+//  Copyright © 2018 储诚鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ __attribute__/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  __attribute__
+//
+//  Created by 储诚鹏 on 2018/10/12.
+//  Copyright © 2018 储诚鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
